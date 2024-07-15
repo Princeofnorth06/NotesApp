328d3ad0aa4116868eeced944ed826e997951b40
princeofnorth06 <princedubey80066@gmail.com>
2024-07-15 12:19:38 +0530

    sdd

--- noteapp/lib/services/api_services.dart
+++ noteapp/lib/services/api_services.dart
@@ -7,15 +7,11 @@ import 'package:http/http.dart' as http;
 class ApiServices {
   static String _baseUrl = "https://notesapp-suda.onrender.com/notes";
 
-  // Replace with your actual base URL
-
   static Future<void> addNote(Note note) async {
     Uri requestUri = Uri.parse("$_baseUrl/add");
 
-    // Ensure the body is encoded as a JSON string
     var body = jsonEncode(note.toMap());
 
-    // Log the request body for debugging
     log('Request body: $body');
 
     try {
@@ -27,11 +23,9 @@ class ApiServices {
         body: body,
       );
 
-      // Log the response details for debugging
       log('Response status: ${response.statusCode}');
       log('Response body: ${response.body}');
 
-      // Check if the response is successful
       if (response.statusCode == 200) {
         var decode = jsonDecode(response.body);
         log('Decoded response: $decode');
@@ -46,10 +40,8 @@ class ApiServices {
   static Future<void> deleteNote(Note note) async {
     Uri requestUri = Uri.parse("$_baseUrl/delete");
 
-    // Ensure the body is encoded as a JSON string
     var body = jsonEncode(note.toMap());
 
-    // Log the request body for debugging
     log('Request body: $body');
 
     try {
@@ -61,11 +53,9 @@ class ApiServices {
         body: body,
       );
 
-      // Log the response details for debugging
       log('Response status: ${response.statusCode}');
       log('Response body: ${response.body}');
 
-      // Check if the response is successful
       if (response.statusCode == 200) {
         var decode = jsonDecode(response.body);
         log('Decoded response: $decode');
@@ -80,10 +70,8 @@ class ApiServices {
   static Future<List<Note>> fetchNote(String userId) async {
     Uri requestUri = Uri.parse("$_baseUrl/list");
 
-    // Encode the request body as a JSON string
     var body = jsonEncode({"userid": userId});
 
-    // Log the request body for debugging
     log('Request body: $body');
 
     try {
@@ -95,16 +83,13 @@ class ApiServices {
         body: body,
       );
 
-      // Log the response details for debugging
       log('Response status: ${response.statusCode}');
       log('Response body: ${response.body}');
 
-      // Check if the response is successful
       if (response.statusCode == 200) {
         var decode = jsonDecode(response.body);
         log('Decoded response: $decode');
 
-        // Map the decoded response to a list of Note objects
         List<Note> notes = [];
         for (var noteMap in decode) {
           Note newNote = Note.fromMap(noteMap);
